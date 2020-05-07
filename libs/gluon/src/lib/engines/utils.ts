698b7685d0d5f79937e7902f34b2b593cc17bbc7
terceser <7141065+terceser@users.noreply.github.com>
2020-05-07 12:21:57 +0200

    feat (utils): add align time series

--- libs/gluon/src/lib/engines/utils.ts
+++ libs/gluon/src/lib/engines/utils.ts
@@ -19,3 +19,35 @@ export const sortTimeSeries = (
     (r1, r2) => op * (r1.date.getTime() - r2.date.getTime())
   );
 };
+
+/**
+ * Align time series list
+ * @param seriesList series list to be aligned
+ */
+export const alignTimeSeries = (...seriesList: TimeSeries[]) => {
+  if (!seriesList) {
+    return;
+  }
+
+  if (seriesList.length < 2) {
+    return seriesList;
+  }
+
+  const results: TimeSeries[] = new Array(seriesList.length).fill([]);
+
+  const ref = sortTimeSeries(seriesList[0]);
+
+  results[0] = ref;
+
+  for (let seriesIndex = 0; seriesIndex < ref.length; seriesIndex++) {
+    for (let i = 1; i < seriesList.length; i++) {
+      results[seriesIndex].push(
+        seriesList[seriesIndex].find(
+          s => s.date.getTime() === ref[i].date.getTime()
+        )
+      );
+    }
+  }
+
+  return results;
+};
